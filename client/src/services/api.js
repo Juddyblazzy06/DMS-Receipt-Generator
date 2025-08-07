58c0c510a75e339b572d7cd07edc9daa1a75d468
juddyblazzy06 <amiensjude@gmail.com>
2025-08-07 01:56:19 +0100

    remove client credentials

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -5,7 +5,6 @@ const API_BASE_URL = BACKEND_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
-  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
